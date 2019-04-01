9bd7c8076b927a3a3f02cd57c26e9b4d947f6bff
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-04-01 16:38:01 +1100

    update tests and use a safer version of min/max that returns NA when object is length 0 (rather than Inf/-Inf)

---
+++ R/utils.R
@@ -0,0 +1,24 @@
+#' Calculate maximum/minimum and return NA if input is empty
+#'
+#' This is a helper function that returns NA if length(x) == 0 (e.g., is 
+#' numeric(0)), otherwise calculates the maximum/minimum
+#'
+#' @param x numeric
+#' @param ... additional arguments for min/max
+#'
+#' @return
+#' @examples
+#' max_if(numeric(0))
+#' @name safe_minima
+max_if <- function(x, ...){
+  ifelse(test = length(x) == 0,
+         yes = NA,
+         no = max(x, ...))
+}
+
+#' @rdname safe_minima
+min_if <- function(x, ...){
+  ifelse(test = length(x) == 0,
+         yes = NA,
+         no = min(x, ...))
+}
