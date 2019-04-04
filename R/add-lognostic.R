ce76799c909dfb10789b2e2cf83c91b9a418c77a
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-04-04 14:30:28 +1100

    created first add_l_ funciton to add maximum. Working towards #6

---
+++ R/add-lognostic.R
@@ -0,0 +1,32 @@
+#' Add lognostic to data
+#' 
+#' Adds a lognostic to the original dataframe
+#'
+#' @param data dataframe
+#' @param id id that uniquely identifies individual
+#' @param var variable to calculate summary of
+#'
+#' @return dataframe with lognostic
+#' @name add-lognostic
+#' @export
+#'
+#' @examples
+#' add_l_max(data = wages,
+#'           id = id,
+#'           var = lnw)
+add_l_max <- function(data,
+                      id,
+                      var){
+  
+  quo_id <- rlang::enquo(id)
+  quo_var <- rlang::enquo(var)
+  
+  str_id <- rlang::as_label(rlang::enquo(id))
+
+l_max(data,
+      !!quo_id,
+      !!quo_var) %>%
+  dplyr::left_join(data,
+                   by = str_id)
+
+}
