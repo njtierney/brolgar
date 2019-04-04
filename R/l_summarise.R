aa01562124e5821563544bb4d7634699c4bfd7f3
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-04-04 17:34:17 +1100

    first attempt at create many lognostics

---
+++ R/l_summarise.R
@@ -0,0 +1,30 @@
+# 
+# l_summarise(wages,
+#             id = id,
+#             .vars = lnw,
+#             funs = list(min = min, 
+#                         max = max, 
+#                         mean = mean))
+
+l_summarise <- function(data,
+                        id,
+                        .vars,
+                        funs){
+  
+  quo_id <- rlang::enquo(id)
+  quo_var <- rlang::enquo(.vars)
+  
+  # rename the function names
+  if (is.null(names(funs))) {
+    
+  } 
+  
+  funs_names <- names(funs)
+  glued_names <- glue::glue("l_{funs_names}")
+  l_funs <- rlang::set_names(funs,  glued_names)
+  
+  data %>%
+    dplyr::group_by(id) %>%
+    dplyr::summarise_at(.vars = dplyr::vars(lnw),
+                        .funs = l_funs)
+}
