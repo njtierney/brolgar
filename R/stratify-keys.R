552d313e864ba8e475b4f1f274b30fdedb8f7ebf
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-07-23 17:54:32 +1000

    implement along argument in stratify_keys

--- R/stratify-keys.R
+++ R/stratify-keys.R
@@ -2,12 +2,14 @@
 #'
 #' @param .data data.frame to explore
 #' @param n_strata number of random groups to create
-#' @param along variable to stratify along
+#' @param along variable to stratify along. This groups by each `key` and then 
+#'   takes a summary statistic (by default, the mean). It then arranges by the 
+#'   mean value for each `key` and assigns the `n_strata` groups.
+#' @param fun summary function
 #' @param ... extra arguments
 #'
 #' @return data.frame with column, `.strata` containing `n_strata` groups
 #' @export
-#'
 #' @examples
 #' library(ggplot2)
 #' library(brolgar)
@@ -20,7 +22,32 @@
 #'             group = id)) + 
 #'  geom_line() + 
 #'  facet_wrap(~.strata)
-stratify_keys <- function(.data, n_strata, along = NULL, ...){
+#'  
+#'  # now facet along some feature
+#'  
+#' library(dplyr)
+#'  wages_ts %>%
+#' key_slope(ln_wages ~ xp) %>%
+#'   right_join(wages_ts, ., by = "id") %>%
+#'   stratify_keys(n_strata = 12,
+#'                 along = .slope_xp,
+#'                 fun = median) %>%
+#'   ggplot(aes(x = xp,
+#'              y = ln_wages,
+#'              group = id)) + 
+#'   geom_line() + 
+#'   facet_wrap(~.strata)
+#' 
+#' 
+#' wages_ts %>%
+#'   stratify_keys(n_strata = 12,
+#'                 along = unemploy_rate) %>%
+#'   ggplot(aes(x = xp,
+#'              y = ln_wages,
+#'              group = id)) + 
+#'   geom_line() + 
+#'   facet_wrap(~.strata)
+stratify_keys <- function(.data, n_strata, along = NULL, fun, ...){
   test_if_tsibble(.data)
   test_if_null(.data)
   UseMethod("stratify_keys")
@@ -28,7 +55,11 @@ stratify_keys <- function(.data, n_strata, along = NULL, ...){
 }
  
 #' @export
-stratify_keys.tbl_ts <- function(.data, n_strata, along = NULL, ...){
+stratify_keys.tbl_ts <- function(.data, 
+                                 n_strata, 
+                                 along = NULL, 
+                                 fun = mean,
+                                 ...){
   
   q_along <- rlang::enquo(along)
   
@@ -39,22 +70,28 @@ stratify_keys.tbl_ts <- function(.data, n_strata, along = NULL, ...){
   full_strata <- rep.int(possible_strata,
                          times = lengths(tsibble::key_rows(.data)))
 
-  if (is.null(q_along)) {
-    .data %>%
+  if (rlang::quo_is_null(q_along)) {
+    
+    data_strata <- .data %>%
       dplyr::mutate(.strata = full_strata)
+    
+    return(data_strata)
   } 
   
-  if (!is.null(q_along)) {
+  if (!rlang::quo_is_null(q_along)) {
     
-    full_strata <- sort(full_strata)
+    data_strata <- .data %>%
+      tibble::as_tibble() %>%
+      dplyr::group_by(!!!tsibble::key(.data)) %>%
+      dplyr::summarise(stat = fun(!!q_along, na.rm = TRUE)) %>%
+      dplyr::arrange(-stat) %>%
+      dplyr::mutate(.strata = sort(possible_strata)) %>%
+      dplyr::select(-stat) %>%
+      dplyr::right_join(.data,
+                        .,
+                        by = tsibble::key_vars(.data))
     
-    returned_data <- .data %>%
-      dplyr::arrange(!!!tsibble::key(.data), 
-                     !!tsibble::index(.data),
-                     !!q_along) %>%
-      dplyr::mutate(.strata = full_strata)
-    
-    return(returned_data)
+    return(data_strata)
   
   }
   
