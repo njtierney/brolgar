89a9f2e0ae51efea56291def1a75b5912e49c638
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-09-11 10:31:51 +1000

    add some development code for a wider reflective palette

--- R/pal-reflect.R
+++ R/pal-reflect.R
@@ -122,3 +122,95 @@ scale_colour_reflective <- function(palette = "hawaii",
 # scico_palette_show(scico_seq)
 # 
 # 
+
+
+# library(dplyr)
+# library(ggplot2)
+# library(scico)
+# library(brolgar)
+# 
+# heights_near <- key_slope(heights,
+#                           height_cm ~ year) %>%
+#   keys_near(key = country,
+#             var = .slope_year) %>%
+#   left_join(heights, by = "country")
+# 
+# ggplot(heights_near,
+#        aes(x = year,
+#            y = height_cm,
+#            group = country,
+#            colour = stat)) +
+#   geom_line() +
+#   scale_colour_reflective(palette = "batlow",
+#                           pal_dir = "left")
+# 
+# scico_reflect <- function(n,
+#                           alpha = NULL,
+#                           direction = 1,
+#                           palette = "bilbao"){
+#   
+#   # new_n <- 
+#   
+#   pal_lhs <- scico::scico(n = n,
+#                           alpha = alpha,
+#                           begin = 0,
+#                           end = 1,
+#                           direction = direction,
+#                           palette = palette)
+#   
+#   pal_rhs <- scico::scico(n = n,
+#                           alpha = alpha,
+#                           begin = 1,
+#                           end = 0,
+#                           direction = direction,
+#                           palette = palette)
+#   
+#   c(pal_lhs, pal_rhs)
+#   
+# }
+# 
+# 
+# scico_discrete_reflect <- function(alpha, 
+#                                    direction,
+#                                    palette){
+#   function(n) {
+#     scico_reflect(n, 
+#                   alpha, 
+#                   direction, 
+#                   palette)
+#   }
+# }
+# 
+# scale_colour_reflect_d <- function(...,
+#                                    alpha = 1,
+#                                    direction = 1,
+#                                    palette = "bilbao",
+#                                    aesthetics = "colour"){
+#   
+#   if (!requireNamespace("ggplot2", quietly = TRUE)) {
+#     stop("ggplot2 is required for this functionality", call. = FALSE)
+#   }
+#   
+#   if (alpha < 0 | alpha > 1) {
+#     stop("alpha must be in [0,1]")
+#   }
+#   
+#   if (!palette %in% scico_palette_names()) {
+#     stop("Need to pick a scico palette. see `scico_palette_names() for options")
+#   }
+#   
+#   ggplot2::discrete_scale(aesthetics, 
+#                           "reflective_d", 
+#                           scico_discrete_reflect(alpha, 
+#                                                  direction, 
+#                                                  palette), 
+#                           ...)
+# }
+# 
+# ggplot(heights_near,
+#        aes(x = year,
+#            y = height_cm,
+#            group = country,
+#            colour = stat)) +
+#   geom_line() +
+#   scale_colour_reflect_d(palette = "bilbao")
