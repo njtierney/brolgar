7e1c1aadf598663409568653463baaff19d5bd09
Nicholas Tierney <nicholas.tierney@gmail.com>
2020-10-13 12:55:30 +1100

    add tests for index summaries

---
+++ tests/testthat/test-index-helpers.R
@@ -0,0 +1,25 @@
+test_that("index regular returns logical", {
+  expect_type(index_regular(pisa, year), "logical")
+})
+
+test_that("index regular fails when non data.frame provided", {
+  expect_error(index_regular(matrix(0), 1))
+})
+
+test_that("index regular fails when column doesn't exist", {
+  expect_error(index_regular(pisa, wat))
+})
+
+test_that("index summary returns summaryDefault", {
+  expect_is(index_summary(pisa, year), "summaryDefault")
+  expect_type(index_summary(pisa, year), "double")
+})
+
+test_that("index summary fails when non data.frame provided", {
+  expect_error(index_summary(matrix(0), 1))
+})
+
+test_that("index summary fails when column doesn't exist", {
+  expect_error(index_summary(pisa, wat))
+})
+
